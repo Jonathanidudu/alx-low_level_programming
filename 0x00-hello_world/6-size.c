e4f25c7c283864829957736b117ffdd75aee8495
Jonathanìdudu <jonathanidudu@gmail.com>
2022-10-22 06:52:26 -0700

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+printf("size of a char :%lu byte(s)\n", sizeof(char));
+
+printf("size of an int : %lu byte(s)\n", sizeof(short int));
+
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+
+printf("size of long long int : %lu byte(s)\n", sizeof(long long int));
+
+printf("size of float : %lu byte(s)\n",  sizeof(float));
+
+return (0);
+
+}
