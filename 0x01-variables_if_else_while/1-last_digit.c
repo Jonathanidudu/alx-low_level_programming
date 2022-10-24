a58220115a1be1a9d4fdce2db8beae6341ea7e56
Jonathanìdudu <jonathanidudu@gmail.com>
2022-10-24 03:21:22 -0700

    This programme prints the last digit of a random number and states if the number is greater than 5, is 0 or is less than 6 but not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,20 +23,20 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-int  last_digit;
-last_digit = abs(n) % 10;
+int  digit;
+digit = abs(n) % 10;
 
-if (last_digit < 0)
-last_digit = last_digit *(-1);
+if (digit < 0)
+digit = digit *(-1);
 
-if (last_digit > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+if (digit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 
-else if (last_digit == 0)
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
+else if (digit == 0)
+printf("Last digit of %d is %d and is 0\n", n, digit);
 
 else
-printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0", n, digit);
 
 return (0);
 }
