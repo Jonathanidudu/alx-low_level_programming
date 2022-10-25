0b6775d6fbb77f1a9b777595b4ee86b3aefb68b6
Jonathanìdudu <jonathanidudu@gmail.com>
2022-10-24 19:22:59 -0700

    getting all single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+
+/**
+ *main - entry point to the programme
+ *
+ *Description: A programme that prints all single digits in
+ *base 10
+ *
+ *Return: 0
+ *
+ */
+
+int main(void)
+{
+int j;
+
+{
+for (j = 0; j <= 9; j++)
+printf("%d", j);
+}
+
+printf("\n");
+return (0);
+}
